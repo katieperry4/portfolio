02124e10f6f27878f37891a4831bf10b4a626a76
Katie Perry <katelynnperry99@gmail.com>
2024-03-25 09:43:01 +0000

    updating cv link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -73,7 +73,7 @@ const Contact = () => {
                 target="_blank"
                 rel="noreferrer"
                 className="nav-li text-xl"
-                href="https://drive.google.com/file/d/1cqWyJVQVwmmpPdsEx2XNQL_ceM8YgZym/view?usp=sharing"
+                href="https://drive.google.com/file/d/1OyitRv6b9BQ3qcM-SKeZVHMinAdg9OK3/view?usp=sharing"
               >
                 <img
                   className="inline contact-icon"
